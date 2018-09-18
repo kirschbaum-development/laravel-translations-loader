9a86902412df204f029e2b8f5ae477d6fc0d070e
Luis Dalmolin <luis.nh@gmail.com>
2018-09-18 15:29:53 -0300

    Added the ability to load from different locations

--- json.js
+++ json.js
@@ -5,12 +5,19 @@ const jsonLoader = require('./json-loader');
 
 module.exports = function (indexContent) {
     let options = {};
+    let baseDirectory;
 
     try {
         options = loaderUtils.parseQuery(this.query);
     } catch (e) { }
 
-    const baseDirectory = path.dirname(this.resource) + '/../../../resources/lang';
+    if (path.dirname(this.resource) === path.dirname('node_modules/@kirschbaum-development/laravel-translations-loader/json.js')) {
+        baseDirectory = path.dirname(this.resource) + "/../../../resources/lang";
+    } else {
+        baseDirectory = path.dirname(this.resource);
+    }
+
+    this.addDependency(baseDirectory);
 
     return "module.exports = " + JSON.stringify(
         jsonLoader.execute(baseDirectory, options)
--- php.js
+++ php.js
@@ -6,12 +6,19 @@ const loaderUtils = require('loader-utils');
 
 module.exports = function (indexContent) {
     let options = {};
+    let baseDirectory;
 
     try {
         options = loaderUtils.parseQuery(this.query);
     } catch (e) { }
 
-    const baseDirectory = path.dirname(this.resource) + '/../../../resources/lang';
+    if (path.dirname(this.resource) === path.dirname('node_modules/@kirschbaum-development/laravel-translations-loader/php.js')) {
+        baseDirectory = path.dirname(this.resource) + "/../../../resources/lang";
+    } else {
+        baseDirectory = path.dirname(this.resource);
+    }
+
+    this.addDependency(baseDirectory);
 
     return "module.exports = " + JSON.stringify(
         phpLoader.execute(baseDirectory, options)
